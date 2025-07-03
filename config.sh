a74377106b2cf48fd03774a714f253b5dfbc8a1b
Anmol Khirbat <anmol@khirbat.net>
2025-07-03 10:48:56 -0700

    Update debian 12 image

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 KTHW_PI_HOST=5a
 KTHW_SSH_CA_KEY="$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/PublicKeys/7c66b4c7decda51b23caf600d9d379f3.pub"
-KTHW_DEBIAN_IMAGE="https://cloud.debian.org/images/cloud/bookworm/20250530-2128/debian-12-generic-arm64-20250530-2128.qcow2"
+KTHW_DEBIAN_IMAGE="https://cloud.debian.org/images/cloud/bookworm/20250530-2128/debian-12-genericcloud-arm64-20250530-2128.qcow2"
 KTHW_POD_CIDR0=10.200.0.0/24
 KTHW_POD_CIDR1=10.200.1.0/24
 KTHW_APT_CACHER_NG="http://192.168.1.2:3142"
